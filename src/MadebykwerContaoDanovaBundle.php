f3eea53be256f60dbeb27361d1d41e14b21e0f3f
Sead Dedić <46990331+daesign@users.noreply.github.com>
2025-12-16 09:01:01 +0100

    Update MadebykwerContaoDanovaBundle.php

--- src/MadebykwerContaoDanovaBundle.php
+++ src/MadebykwerContaoDanovaBundle.php
@@ -3,7 +3,33 @@
 namespace Madebykwer\ContaoDanova;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\CoreBundle\ContaoCoreBundle;
 
-class MadebykwerContaoDanovaBundle extends Bundle
+class MadebykwerContaoDanovaBundle extends Bundle implements BundlePluginInterface
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/Resources/config')
+        );
+
+        $loader->load('services.yaml');
+    }
+
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(self::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
 }
